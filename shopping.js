5c3ca2e205bc4e2e2128dcca23e5a337a94901b9
AlexLam12 <alex.e015@gmail.com>
2021-06-01 12:04:37 -0500

    finished shopping list

--- shopping.js
+++ shopping.js
@@ -3,36 +3,43 @@ const groceries = [
         item: "milk",
         id: 1,
         price: 3,
+        dateCreated: new Date,
     },
     {
         item: "eggs",
         id: 2,
         price: 3, 
+        dateCreated: new Date,
     },
     {
         item: "pizza Rolls",
         id: 3,
         price: 5, 
+        dateCreated: new Date,
     },
     {
         item: "beans",
         id: 4,
         price: 1, 
+        dateCreated: new Date,
     },
     {
         item: "grapefruit",
         id: 5,
         price: 2, 
+        dateCreated: new Date,
     },
     {
         item: "sandwitch Buns",
         id: 6,
         price: 4, 
+        dateCreated: new Date,
     },
     {
         item: "condiments",
         id: 7,
         price: 6, 
+        dateCreated: new Date
     }
     ]
 
@@ -52,22 +59,27 @@ const addToShoppingList = (itemObject) => {
 const tomatos = {
     item: "tomatos",
     price: 4,
+    dateCreated: new Date,
 }
 const rice = {
     item: "rice",
     price: 6,
+    dateCreated: new Date,
 }
 const cerial = {
     item: "cerial",
     price: 4,
+    dateCreated: new Date,
 }
 const steak = {
     item: "steak", 
     price: 10,
+    dateCreated: new Date,
 }
 const chicken = {
     item: "chichen",
     price: 8,
+    dateCreated: new Date,
 }
 addToShoppingList(cerial)
 addToShoppingList(tomatos)
@@ -75,4 +87,10 @@ addToShoppingList(rice)
 addToShoppingList(steak)
 addToShoppingList(chicken)
 
-console.log(groceries)
+for (item of groceries) {
+    if (item.price > 8) {
+    console.log(item)
+}
+}
+
+// console.log(groceries)
